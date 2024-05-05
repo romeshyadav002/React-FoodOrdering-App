305cbc1cca4058ab179c991590839a797046bead
Romesh Yadav <romesh.yadav@jumbaya.com>
2024-05-05 21:10:07 +0530

    lecture 2 build issues resolved

--- App.js
+++ App.js
@@ -1,35 +1,20 @@
-import React from 'react'
-import  ReactDOM from "react-dom/client";
+import React from 'react';
+import { ReactDOM, createRoot } from 'react-dom/client';
+
+//React.createElement is a JS Object
 const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "Heading 1"
-  );
-  
-  const heading2 = React.createElement(
-    "h2",
-    {
-      id: "title",
-    },
-    "Heading 2"
-  );
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading2]
-  );
-  
-  console.log(heading);
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  //passing a react element inside the root
-  
-  //async defer
-  root.render(container);
-  
+    "div", //Which tag to be created?
+    {id : "parent"}, //attributes
+    React.createElement("div", {id : "child"}, 
+    [React.createElement("h1", {id : "child1"}, "I'm a h1 tag"),
+    React.createElement("h2", {id : "child2"}, "I'm a h2 tag")])
+);//what to display?
+
+//Rendering react
+const root = createRoot(document.getElementById("root"));
+
+/*
+render() => renders a JS object(heading) and 
+creates the tag which browser understands puts it up in the DOM.
+*/
+root.render(heading);
